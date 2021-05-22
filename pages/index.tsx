3b347bf634df80e4d1fbc406adf1a8b79c7d420a
igorWebMASTER <igor_1917@hotmail.com>
2021-05-22 14:25:20 -0300

    feat: add search component

---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import styles from '../styles/Home.module.css'
+import { useState, FormEvent } from 'react'
+import { SearchResults } from '../components/SearchResults'
+
+export default function Home() {
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if(!search.trim()){
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    console.log(data)
+
+
+    setResults(data)
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSearch}>
+        <input 
+            type="text" 
+            name="search" 
+            value={search} 
+            onChange={e => setSearch(e.target.value)} 
+        />
+        <button type="submit">Buscar</button>
+      </form> 
+
+      <SearchResults  results={results} />
+    </div>
+  )
+}
